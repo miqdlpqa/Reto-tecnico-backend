f873499156cf536d97af472fde047a383af29049
miqdlpqa <miquel.delapiedra@visma.com>
2026-03-08 21:28:29 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
-use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/db-test', function () {
-    DB::connection()->getPdo();
-    return "DB conectada";
+Route::get('/', function () {
+    return 'Backend OK';
 });
