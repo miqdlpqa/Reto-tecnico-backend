3843685c8e1886d3c9aa039a53d635c913d14ff0
miqdlpqa <miquel.delapiedra@visma.com>
2026-03-08 21:41:57 -0500

    Replace /db-test route with /tables route

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/db-test', function () {
-    try {
-        DB::connection()->getPdo();
-        return response()->json([
-            "status" => "ok",
-            "message" => "DB conectada correctamente"
-        ]);
-    } catch (\Throwable $e) {
-        return response()->json([
-            "status" => "error",
-            "message" => $e->getMessage()
-        ], 500);
-    }
+Route::get('/tables', function () {
+    return DB::select('SHOW TABLES');
 });
