aa1552c4a69e86f552fd65f91dfa2996bf233fd1
miqdlpqa <miquel.delapiedra@visma.com>
2026-03-08 21:58:21 -0500

    Add routes for database migration and seeding

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
-
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/run-migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return nl2br(Artisan::output());
+});
+
+Route::get('/run-seed', function () {
+    Artisan::call('db:seed', ['--force' => true]);
+    return nl2br(Artisan::output());
+});
+
+Route::get('/tables', function () {
+    return response()->json([
+        'database' => DB::getDatabaseName(),
+        'tables' => DB::select('SHOW TABLES')
+    ]);
+});
 
 Route::get('/divisions-data', function () {
     return DB::table('divisions')->get();
