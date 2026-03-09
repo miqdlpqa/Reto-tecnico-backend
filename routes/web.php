ba47064964f2a80183b089f10683dfb369005053
miqdlpqa <miquel.delapiedra@visma.com>
2026-03-08 22:16:38 -0500

    Refactor routes to remove old endpoints and add status check
    Removed old routes for migrations, seeds, and divisions data. Added a new route for the API status.

--- routes/web.php
+++ routes/web.php
@@ -3,38 +3,8 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/run-migrate', function () {
-    Artisan::call('migrate', ['--force' => true]);
-    return nl2br(Artisan::output());
-});
-
-Route::get('/run-seed', function () {
-    Artisan::call('db:seed', ['--force' => true]);
-    return nl2br(Artisan::output());
-});
-
-Route::get('/tables', function () {
+Route::get('/', function () {
     return response()->json([
-        'database' => DB::getDatabaseName(),
-        'tables' => DB::select('SHOW TABLES')
+        'status' => 'API running'
     ]);
-});
-
-Route::get('/divisions-data', function () {
-    return DB::table('divisions')->get();
-});
-
-Route::get('/count/divisions', function () {
-    return [
-        'count' => DB::table('divisions')->count()
-    ];
-});
-
-Route::get('/run-division-seeder', function () {
-    Artisan::call('db:seed', [
-        '--class' => 'Database\\Seeders\\DivisionSeeder',
-        '--force' => true,
-    ]);
-
-    return nl2br(Artisan::output());
 });
